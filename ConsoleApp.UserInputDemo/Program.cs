6d22c63cf959588eee6acda969a5c8bfbf958029
Naveen Kumar <er.crackerjack@gmail.com>
2024-09-12 12:42:40 +1200

    Add project files.

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
+Console.WriteLine("Hello, World!");
+
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());   
+
+int workingYearsLeft = retirementAge - age;
+
+Console.WriteLine($"Full name {name}");
+Console.WriteLine($"Current age {age}");
+Console.WriteLine($"Retirement years left {workingYearsLeft}");
+
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Different Datatypes
+/*
+ * text - String
+ * integers - Int
+ * decimal - double, float, decimal
+ * logical - bool
+ */
+
+String name = "Naveen Kumar";
+Console.WriteLine("I am "+ name);// String concatenation
+Console.WriteLine($"I am {name}");// String interpolation
+
+int age = 90;
+Console.WriteLine(age);
+int retirementAgeLeft = 11;
+int retirementAge = age + retirementAgeLeft; 
+Console.WriteLine($" Retirement age is { age + retirementAgeLeft}");
+
+bool isRetired = false;
+Console.WriteLine($"Am i retired? {isRetired}");
