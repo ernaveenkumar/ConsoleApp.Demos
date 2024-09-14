39f0e13253369171b69874d27218ae3a1fee627f
Naveen Kumar <er.crackerjack@gmail.com>
2024-09-14 20:04:48 +1200

    Added exception hadling.

--- ConsoleApp.UserInputDemo/Program.cs
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -3,11 +3,15 @@ using System.Xml;
 
 Console.WriteLine("Hello, World!");
 
-string firstName = string.Empty;
-string lastName = string.Empty;
-
+string? firstName = string.Empty;
+string? lastName = string.Empty;
 int age = 0;
 int retirementAge = 65;
+decimal salary;
+char gender = char.MinValue;
+bool working = true;
+
+
 Console.WriteLine("Please enter your name");
 firstName = Console.ReadLine();
 
@@ -15,11 +19,25 @@ Console.WriteLine("Please enter your last name");
 lastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age");
-age = Convert.ToInt32(Console.ReadLine());   
+age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please eneter your salary");
+salary = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Please enter your gender");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Are you working ?! Please enter tru of false");
+working = Convert.ToBoolean(Console.ReadLine());
 
 int workingYearsLeft = retirementAge - age;
 
-Console.WriteLine($"Full name {name}");
+Console.WriteLine($"Full name {firstName} {lastName}");
 Console.WriteLine($"Current age {age}");
+Console.WriteLine($"Your salary is {salary}");
+Console.WriteLine($"Your working status is {working}");
+Console.WriteLine($" Your gender is {gender}");
 Console.WriteLine($"Retirement years left {workingYearsLeft}");
+
+
 
---
+++ SampleCalculator/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more informationConsole.WriteLine("-Welcome to the sample calculator");
+
+//Show calculatoe options
+
+using System.Xml.Serialization;
+int choice = 0;
+int num1, num2, answer = 0;
+
+while (choice != -1)
+{
+    Console.WriteLine("If enter any key to continue and enter -1 to exit");
+    Console.WriteLine("Please select an option");
+    Console.WriteLine("1. Addition");
+    Console.WriteLine("2. Subtration");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+
+    try
+    {
+        choice = Convert.ToInt32(Console.ReadLine());
+    }catch (Exception)
+    {
+        Console.WriteLine("Invalid input, press any key to continue");
+        Console.ReadLine();
+        Console.Clear();    
+        continue;
+    }
+    
+
+    if (choice == -1) {
+        break;
+    }
+        
+    Console.WriteLine("Please enter the firstNumber");
+    try
+    {
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Invalid Input");
+        continue;
+    }
+        
+
+    Console.WriteLine("Please enter the secondNumber");
+    try
+    {
+       num2 = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Invalid Output");
+        throw;
+    }
+
+        answer = 0;
+    
+            switch (choice)
+            {
+
+                case 1:
+                    Console.WriteLine("Addiition");
+                    answer = num1 + num2;
+                    break;
+                case 2:
+                    Console.WriteLine("Subtraction");
+                    answer = num1 - num2;
+                    break;
+                case 3:
+                    Console.WriteLine("Multiplicatiojn");
+                    answer = num1 * num2;
+                    break;
+                case 4:
+
+            try
+            {
+                Console.WriteLine("Division");
+                answer = num1 / num2;
+                break;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Number cannot be divided by zero");
+            }
+            catch (Exception) {
+                throw;
+            }
+                break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    Console.WriteLine("Press Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+            }
+        
+        Console.WriteLine($"Output is : {answer}"); Console.WriteLine("Hello, World!");
+} 
+
